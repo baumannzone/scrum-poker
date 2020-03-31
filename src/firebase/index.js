d5e0792c6110e6b2faaa522da17d138391cf8543
Jorge Baumann <jorge.baumann.aguilar@gmail.com>
2020-03-31 14:10:07 +0200

    Initial Commit
    Bootstrap-vue + Firebase

---
+++ src/firebase/index.js
@@ -0,0 +1,20 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import * as firebase from 'firebase/app'
+
+// Add the Firebase products that you want to use
+import 'firebase/firestore'
+
+const apiKey = process.env.VUE_APP_FIREBASE_API_KEY
+
+const firebaseConfig = {
+  apiKey,
+  authDomain: 'scrum-poker-vue.firebaseapp.com',
+  databaseURL: 'https://scrum-poker-vue.firebaseio.com',
+  projectId: 'scrum-poker-vue',
+  storageBucket: 'scrum-poker-vue.appspot.com',
+  messagingSenderId: '816866661794',
+  appId: '1:816866661794:web:2312b188366cb0123e4efb',
+  measurementId: 'G-XT3GR2T7PS'
+}
+
+firebase.initializeApp(firebaseConfig)
